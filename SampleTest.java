7835b3e68e535ee8ae21783e94136fa2d1b721ed
rmallamp <36901144+rmallamp@users.noreply.github.com>
2018-03-27 10:33:28 -0400

    Update SampleTest.java

--- SampleTest.java
+++ SampleTest.java
@@ -4,7 +4,12 @@ public class SampleTest {
   public static int addInt(int a, int b) {
       
       return a+b;
-      
    }
+  
+  public static int subtractInt(int a, int b) {
+    
+      return b - a;
+    
+  }
    
 }
