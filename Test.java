754df5bdeb898b28e3598bac8bd0501973c499ef
rmallamp <36901144+rmallamp@users.noreply.github.com>
2018-03-27 10:22:00 -0400

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Test {
+  public static void main(String[] args) {
+    
+      //The main function just prints a system.out
+      
+      System.out.println(" This is a GitHub test");
+  return;    
+    }
+}
